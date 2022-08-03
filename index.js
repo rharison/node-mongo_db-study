33c64fbd05df0082a3d230c705067e86ef9c6552
@rharison <rharison.abreu@gmail.com>
2022-08-03 10:25:18 -0300

    feat: create CRUD

--- index.js
+++ index.js
@@ -11,21 +11,58 @@ function setEnv(){
   })
 }
 
-async function listDatabases(client) {
-  const databasesList = await client.db().admin().listDatabases();
-  console.log('---------- DTABASES ----------')
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+async function createInColletion(collection) {
+  const newDataLocation = {
+    mainName: "Lar da Criança Ninho de Paz",
+    collectionDate: "2022-08-03T03:29:40.798Z",
+    url: "https://www.google.com/maps/search/Lar+da+Crian%C3%A7a+Ninho+de+Paz+-+Pra%C3%A7a+Heitor+Levi,+7+-+Tatuap%C3%A9,+S%C3%A3o+Paulo+-+SP,+03316-070",
+    collectionSuccess: true,
+  }
+
+  await collection.insertOne(newDataLocation)
 }
 
-async function connectDB() {
+async function retrieveInColletion(collection) {
+  const query = { mainName: 'Lar da Criança Ninho de Paz' };
+  const location = await collection.findOne(query)
+  console.log(location)
+}
+
+async function updateInColletion(collection) {
+  const query = { mainName: 'Lar da Criança Ninho de Paz' };
+  await collection.updateOne(
+    query,
+    {
+      $set: { collectionSuccess: true, collectionDate: new Date() },
+    }
+  );
+}
+
+async function deleteInColletion(collection) {
+  const query = { mainName: 'Lar da Criança Ninho de Paz' };
+  await collection.deleteOne(query);
+}
+
+
+async function testeDB(client) {
+  const database = await client.db('scrawping')
+  const location = await database.collection('locations')
+  await createInColletion(location)
+  // await retrieveInColletion(location)
+  // await updateInColletion(location)
+  // await deleteInColletion(location)
+
+}
+
+async function connectMongo() {
   const username = process.env.MONGO_USERNAME;
   const password = process.env.MONGO_PASSWORD;
   const uri = `mongodb+srv://${username}:${password}@scrawping.ztcmq.mongodb.net/?retryWrites=true&w=majority`;
   const client = new MongoClient(uri);
-
+  
   try {
     await client.connect();
-    await listDatabases(client);
+    await testeDB(client);
   } catch (err) {
     console.log('Error in connection: ', err)
   } finally {
@@ -37,5 +74,5 @@ async function connectDB() {
 
 (() => {
   setEnv()
-  connectDB()
+  connectMongo()
 })()
